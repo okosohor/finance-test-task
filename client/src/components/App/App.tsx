e786866d7464d69deb49eb754263d784249ba2f3
Oleksandr Kosohor <kosogor2007@gmail.com>
2023-11-03 16:51:20 +0200

    test

--- client/src/components/App/App.tsx
+++ client/src/components/App/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import './App.css';
 import { TickerList } from '../TickerList';
-import { Filter } from '../Filter';
+import { Filter } from '../Filter/index';
 import { useAppSelector } from '../../hooks';
 
 export const App = () => {
   const tickers = useAppSelector(state => state.tickers.tickers);
   return (
     <>
-      {tickers.length !== 0 && (
-        <Filter />
-      )}
+      {/* {tickers.length !== 0 && ( */}
+      <Filter />
+      {/* // )} */}
       <TickerList />
     </>
   );
